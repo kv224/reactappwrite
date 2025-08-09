cb2fbce5353eddb60940bbd01488b645b32c77a4
kuldeep <vermakuldeep224@gmail.com>
2025-08-09 20:09:05 +0530

    packages installed

--- megatodo/src/App.jsx
+++ megatodo/src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 class="text-3xl font-bold underline text-red-500">
-        Hello world!
+        Blog with appwite
       </h1>
     </>
   )
