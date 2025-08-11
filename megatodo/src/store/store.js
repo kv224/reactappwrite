ae94c65e51df61d69e0bb5509815ae296ad59398
kuldeep <vermakuldeep224@gmail.com>
2025-08-11 22:14:02 +0530

    store js added

---
+++ megatodo/src/store/store.js
@@ -0,0 +1,12 @@
+import {configureStore} from '@reduxjs/toolkit';
+
+
+const store = configureStore({
+    reducer: {
+        // Add your reducers here
+        
+    }
+})
+
+
+export default store;
