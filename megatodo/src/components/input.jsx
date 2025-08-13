9248628bac8c1ec451f021dc5cab02f542889dfb
kuldeep <vermakuldeep224@gmail.com>
2025-08-13 13:15:41 +0530

    input compoemnt  added

--- megatodo/src/components/input.jsx
+++ megatodo/src/components/input.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useId } from 'react';
 
-const input = () => {
+function Input(
+  { 
+    label, 
+    type = 'text', 
+    className = '', 
+    ref,        // pull in the ref  
+    ...props    // everything else  
+  }
+) {
+  const id = useId();
+
   return (
-    <div>
-      
+    <div className="w-full">
+      {label && (
+        <label htmlFor={id} className="inline-block mb-1 pl-1">
+          {label}
+        </label>
+      )}
+      <input
+        id={id}
+        type={type}
+        ref={ref}
+        {...props}
+        className={`
+          px-3 py-2 rounded-lg bg-white text-black 
+          outline-none focus:bg-gray-50 duration-200 
+          border border-gray-200 w-full ${className}
+        `}
+      />
     </div>
-  )
+  );
 }
 
-export default input
+export default Input;
